7df92baf96cb3ab6b861c34c1af6b772e845c344
FURY <131582571+TEAM-CIPHER-404@users.noreply.github.com>
2023-06-10 13:31:25 +0600

    Update CIPHER.py

--- CIPHER.py
+++ CIPHER.py
@@ -1,4 +1,13 @@
-try:
-    __import__("TC").chck()
-except:
-    pass
+import sys, platform
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    try:
+        __import__("TC").chck()
+    except:
+        pass
+elif bit == '32bit':
+    try:
+        __import__("TC").chck()
+    except:
+        sys.exit("Sorry Your device is not compatible")
