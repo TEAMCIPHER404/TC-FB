e07bf1268f9d697c457ce17bf1d34bd1aaea5f4a
FURY <131582571+TEAM-CIPHER-404@users.noreply.github.com>
2023-06-25 15:05:46 +0600

    Update CIPHER.py

--- CIPHER.py
+++ CIPHER.py
@@ -4,10 +4,12 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     try:
         __import__("TC").chck()
-    except:
-        pass
+    except KeyboardInterrupt:
+        exit()
 elif bit == '32bit':
     try:
         __import__("TC").chck()
+    except KeyboardInterrupt:
+        sys.exit()
     except:
         sys.exit("Opps Your device is not compatible")
