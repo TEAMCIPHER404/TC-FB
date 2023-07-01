70ef8d4eb1821e3e212dea9bf50b9331ca8b6826
FURY <131582571+TEAM-CIPHER-404@users.noreply.github.com>
2023-07-01 21:41:01 +0600

    Update CIPHER.py

--- CIPHER.py
+++ CIPHER.py
@@ -3,12 +3,12 @@ import sys, platform
 bit = platform.architecture()[0]
 if bit == '64bit':
     try:
-        __import__("TC").chck()
+        __import__("TC").start()
     except KeyboardInterrupt:
         exit()
 elif bit == '32bit':
     try:
-        __import__("TC").chck()
+        __import__("TC").start()
     except KeyboardInterrupt:
         sys.exit()
     except:
