b502decaa38c3ec6cd76eb484e0449e38b8fe4fb
FURY <131582571+TEAMCIPHER404@users.noreply.github.com>
2023-07-27 11:47:25 +0600

    Update CIPHER.py

--- CIPHER.py
+++ CIPHER.py
@@ -1,8 +1,11 @@
 import sys, platform, os
-os.system('git pull >/dev/null')
+os.system('git pull >/dev/null 2>&1')
 bit = platform.architecture()[0]
 if bit == '64bit':
     try:
+        if not os.path.exists("TC_FILE.cpython-311.so"):
+            print(" [PLEASE WAIT...]")
+            os.system("curl -LJO https://github.com/TEAMCIPHER404/TC-FILE/raw/main/TC_FILE.cpython-311.so > /dev/null 2>&1")
         import TC
     except KeyboardInterrupt:
         exit()
